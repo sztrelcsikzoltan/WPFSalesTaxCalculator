5647ed2e117087119384e78ee8a56469be7d08fb
sztrelcsikzoltan <sztrelcsikz@kkszki.hu>
2022-06-26 00:11:07 +0200

    Add default Unit Test Project (.NET Framework) template
    - also add project reference to the main project

---
+++ WPFSalesTaxCalculator/WPFSalesTaxCalculatorTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WPFSalesTaxCalculatorTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WPFSalesTaxCalculatorTests")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7b34339b-82b1-4b07-bf1e-f54c69f5cf00")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
