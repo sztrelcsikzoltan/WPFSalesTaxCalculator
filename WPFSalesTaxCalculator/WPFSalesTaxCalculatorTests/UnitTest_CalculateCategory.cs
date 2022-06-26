a0fb8ef23efc2c49510b877309d30304f0231c2b
sztrelcsikzoltan <sztrelcsikz@kkszki.hu>
2022-06-27 00:31:07 +0200

    Add test methods for CalculateCategory method - due to test driven development, first the test methods are written, the method itself is not completed yet - for the tests initially we use a fixed value for the actual value, this fixed value will be replaced by the value calculated by the created method in the next step

--- WPFSalesTaxCalculator/WPFSalesTaxCalculatorTests/UnitTest_CalculateCategory.cs
+++ WPFSalesTaxCalculator/WPFSalesTaxCalculatorTests/UnitTest_CalculateCategory.cs
@@ -4,11 +4,54 @@ using System;
 namespace WPFSalesTaxCalculatorTests
 {
     [TestClass]
-    public class UnitTest1
+    public class UnitTest_CalculateCategory
     {
         [TestMethod]
-        public void TestMethod1()
+        public void ExemptCategoryBook()
         {
+            string expected = "book";
+            string actual = "book";
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ExemptCategoryChocolateBar()
+        {
+            string expected = "food";
+            string actual = "food";
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ExemptCategoryBoxOfChocolates()
+        {
+            string expected = "food";
+            string actual = "food";
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ExemptCategoryHeadachePills()
+        {
+            string expected = "medical";
+            string actual = "medical";
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void OtherCategoryMusicCD()
+        {
+            string expected = "other";
+            string actual = "other";
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void OtherCategoryBottleOfParfume()
+        {
+            string expected = "other";
+            string actual = "other";
+            Assert.AreEqual(expected, actual);
         }
     }
 }
