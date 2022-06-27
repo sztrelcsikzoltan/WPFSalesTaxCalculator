dc9c0009dc958296651e5116e27a318d161907ee
sztrelcsikzoltan <sztrelcsikz@kkszki.hu>
2022-06-27 14:39:08 +0200

    Add CalculateCategory method - method to determine the category of a product, since the tax rate varies by category

--- WPFSalesTaxCalculator/WPFSalesTaxCalculator/Classes/Methods.cs
+++ WPFSalesTaxCalculator/WPFSalesTaxCalculator/Classes/Methods.cs
@@ -8,6 +8,23 @@ namespace WPFSalesTaxCalculator.Classes
 {
     public class Methods
     {
-
+        Dictionary<string, string> productByCategory = new Dictionary<string, string>() { { "book", "book" }, { "chocolate", "food" }, { "headache pill", "medical" } };
+        // method to determine the category of a product
+        public string CalculateCategory(string name)
+        {
+            string category = productByCategory.FirstOrDefault(d => name.Contains(d.Key)).Value;
+            /*
+            foreach (var item in productByCategory)
+            {
+                if (name.Contains(item.Key))
+                {
+                    category = item.Value;
+                    break;
+                }
+            }
+            */
+            if (category == null) { category = "other"; }
+            return category;
+        }
     }
 }
