edada0a6813b55daa855576c5d590ccb0b973b53
sztrelcsikzoltan <sztrelcsikz@kkszki.hu>
2022-06-27 15:40:54 +0200

    Change test methods to use CalculateTaxRate method - change static actual values to real values returned by the CalculateTaxRate method - tests were successful

--- WPFSalesTaxCalculator/WPFSalesTaxCalculatorTests/UnitTest_CalculateTaxRate.cs
+++ WPFSalesTaxCalculator/WPFSalesTaxCalculatorTests/UnitTest_CalculateTaxRate.cs
@@ -1,16 +1,19 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using WPFSalesTaxCalculator.Classes;
 
 namespace WPFSalesTaxCalculatorTests
 {
     [TestClass]
     public class UnitTest_CalculateTaxRate
     {
+        Methods method = new Methods();
         [TestMethod]
         public void ExemptAndNotImported()
         {
             double expected = 0.00;
-            double actual = 0.00;
+            // double actual = 0.00;
+            double actual = method.CalculateTaxRate("book", false);
             Assert.AreEqual(expected, actual);
         }
 
@@ -18,7 +21,8 @@ namespace WPFSalesTaxCalculatorTests
         public void ExemptAndImported()
         {
             double expected = 0.05;
-            double actual = 0.05;
+            // double actual = 0.05;
+            double actual = method.CalculateTaxRate("food", true);
             Assert.AreEqual(expected, actual);
         }
 
@@ -26,7 +30,8 @@ namespace WPFSalesTaxCalculatorTests
         public void DefaultAndNotImported()
         {
             double expected = 0.10;
-            double actual = 0.10;
+            // double actual = 0.10;
+            double actual = method.CalculateTaxRate("default", false);
             Assert.AreEqual(expected, actual);
         }
 
@@ -34,7 +39,8 @@ namespace WPFSalesTaxCalculatorTests
         public void DefaultAndImported()
         {
             double expected = 0.15;
-            double actual = 0.15;
+            // double actual = 0.15;
+            double actual = method.CalculateTaxRate("default", true);
             Assert.AreEqual(expected, actual);
         }
 
