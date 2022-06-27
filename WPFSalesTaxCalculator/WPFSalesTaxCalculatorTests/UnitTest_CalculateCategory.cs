bb145d566a166cfb8d6d1165058c1108f047f75b
sztrelcsikzoltan <sztrelcsikz@kkszki.hu>
2022-06-27 14:52:13 +0200

    Change test methods to use CalculateCategory method - change static actual values to real values returned by the CalculateCategory method - tests were successful

--- WPFSalesTaxCalculator/WPFSalesTaxCalculatorTests/UnitTest_CalculateCategory.cs
+++ WPFSalesTaxCalculator/WPFSalesTaxCalculatorTests/UnitTest_CalculateCategory.cs
@@ -1,16 +1,19 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using WPFSalesTaxCalculator.Classes;
 
 namespace WPFSalesTaxCalculatorTests
 {
     [TestClass]
     public class UnitTest_CalculateCategory
     {
+        Methods method = new Methods();
         [TestMethod]
         public void ExemptCategoryBook()
         {
             string expected = "book";
-            string actual = "book";
+            // string actual = "book";
+            string actual = method.CalculateCategory("book");
             Assert.AreEqual(expected, actual);
         }
 
@@ -18,7 +21,8 @@ namespace WPFSalesTaxCalculatorTests
         public void ExemptCategoryChocolateBar()
         {
             string expected = "food";
-            string actual = "food";
+            // string actual = "food";
+            string actual = method.CalculateCategory("chocolate bar");
             Assert.AreEqual(expected, actual);
         }
 
@@ -26,7 +30,8 @@ namespace WPFSalesTaxCalculatorTests
         public void ExemptCategoryBoxOfChocolates()
         {
             string expected = "food";
-            string actual = "food";
+            // string actual = "food";
+            string actual = method.CalculateCategory("box of chocolates");
             Assert.AreEqual(expected, actual);
         }
 
@@ -34,7 +39,8 @@ namespace WPFSalesTaxCalculatorTests
         public void ExemptCategoryHeadachePills()
         {
             string expected = "medical";
-            string actual = "medical";
+            // string actual = "medical";
+            string actual = method.CalculateCategory("packet of headache pills");
             Assert.AreEqual(expected, actual);
         }
 
@@ -42,15 +48,17 @@ namespace WPFSalesTaxCalculatorTests
         public void OtherCategoryMusicCD()
         {
             string expected = "other";
-            string actual = "other";
+            // string actual = "other";
+            string actual = method.CalculateCategory("music CD");
             Assert.AreEqual(expected, actual);
         }
 
         [TestMethod]
-        public void OtherCategoryBottleOfParfume()
+        public void OtherCategoryBottleOfPerfume()
         {
             string expected = "other";
-            string actual = "other";
+            // string actual = "other";
+            string actual = method.CalculateCategory("bottle of perfume");
             Assert.AreEqual(expected, actual);
         }
     }
