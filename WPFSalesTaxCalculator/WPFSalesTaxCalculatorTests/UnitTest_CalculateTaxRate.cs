7c7982c6401aa58e0ec616334ee955feb62ced50
sztrelcsikzoltan <sztrelcsikz@kkszki.hu>
2022-06-27 15:19:52 +0200

    Add test methods for CalculateTaxRate method We test 4 cases: - tax exempt and not imported (tax rate: 0.00) - tax exempt and imported (tax rate: 0.05) - default and not imported (tax rate: 0.10) - default and imported (tax rate: 0.15)

--- WPFSalesTaxCalculator/WPFSalesTaxCalculatorTests/UnitTest_CalculateTaxRate.cs
+++ WPFSalesTaxCalculator/WPFSalesTaxCalculatorTests/UnitTest_CalculateTaxRate.cs
@@ -7,8 +7,36 @@ namespace WPFSalesTaxCalculatorTests
     public class UnitTest_CalculateTaxRate
     {
         [TestMethod]
-        public void TestMethod1()
+        public void ExemptAndNotImported()
         {
+            double expected = 0.00;
+            double actual = 0.00;
+            Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod]
+        public void ExemptAndImported()
+        {
+            double expected = 0.05;
+            double actual = 0.05;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void DefaultAndNotImported()
+        {
+            double expected = 0.10;
+            double actual = 0.10;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void DefaultAndImported()
+        {
+            double expected = 0.15;
+            double actual = 0.15;
+            Assert.AreEqual(expected, actual);
+        }
+
     }
 }
