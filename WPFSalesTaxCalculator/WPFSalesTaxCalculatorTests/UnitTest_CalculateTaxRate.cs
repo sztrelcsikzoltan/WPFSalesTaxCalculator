01f2e0aa24a4dee66f3ed38daa4b263d82d43994
sztrelcsikzoltan <sztrelcsikz@kkszki.hu>
2022-06-27 15:05:05 +0200

    Add UnitTest_CalculateTaxRate.cs to test UnitTest_CalculateTaxRate - before creating the actual method, we create the test methods for it

---
+++ WPFSalesTaxCalculator/WPFSalesTaxCalculatorTests/UnitTest_CalculateTaxRate.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace WPFSalesTaxCalculatorTests
+{
+    [TestClass]
+    public class UnitTest_CalculateTaxRate
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
