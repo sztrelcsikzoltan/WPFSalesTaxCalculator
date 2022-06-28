68a0ab07faa20cd8ed10d37cf2f3efc07ab014bd
sztrelcsikzoltan <sztrelcsikz@kkszki.hu>
2022-06-28 11:30:32 +0200

    Add test methods for CheckRichTextBoxContent method - We test 4 cases: - content of sample basket 1 written into RichTextBox (by ShowSampleBasket method) equals to content read out from it - content of sample basket 2 written into RichTextBox (by ShowSampleBasket method) equals to content read out from it - content of sample basket 3 written into RichTextBox (by ShowSampleBasket method) equals to content read out from it - content of new sample basket in RichTextBox equals to content read out from it

--- WPFSalesTaxCalculator/WPFSalesTaxCalculatorTests/UnitTest_CheckRichTextBoxContent.cs
+++ WPFSalesTaxCalculator/WPFSalesTaxCalculatorTests/UnitTest_CheckRichTextBoxContent.cs
@@ -7,8 +7,43 @@ namespace WPFSalesTaxCalculatorTests
     public class UnitTest_CheckRichTextBoxContent
     {
         [TestMethod]
-        public void TestMethod1()
+        public void ContentSampleBasket1()
         {
+            // in WPF RichTextBox, the lines are separated by the '\r\n>' characters, so it must be considered
+            string basketContent = "> 1 book at 12,49\r\n> 1 music CD at 14,99\r\n> 1 chocolate bar at 0,85\r\n";
+            string richTextBoxContent = "> 1 book at 12,49\r\n> 1 music CD at 14,99\r\n> 1 chocolate bar at 0,85\r\n";
+            bool condition = basketContent == richTextBoxContent;
+            Assert.IsTrue(condition);
+        }
+
+        [TestMethod]
+        public void ContentSampleBasket2()
+        {
+            // in WPF RichTextBox, the lines are separated by the '\r\n>' characters, so it must be considered
+            string basketContent = "> 1 imported box of chocolates at 10,00\r\n> 1 imported bottle of perfume at 47,50\r\n";
+            string richTextBoxContent = "> 1 imported box of chocolates at 10,00\r\n> 1 imported bottle of perfume at 47,50\r\n";
+            bool condition = basketContent == richTextBoxContent;
+            Assert.IsTrue(condition);
+        }
+
+        [TestMethod]
+        public void ContentSampleBasket3()
+        {
+            // in WPF RichTextBox, the lines are separated by the '\r\n>' characters, so it must be considered
+            string basketContent = "> 1 imported bottle of perfume at 27,99\r\n> 1 bottle of perfume at 18,99\r\n> 1 packet of headache pills at 9,75\r\n> 1 box of imported chocolates at 11,25\r\n";
+            string richTextBoxContent = "> 1 imported bottle of perfume at 27,99\r\n> 1 bottle of perfume at 18,99\r\n> 1 packet of headache pills at 9,75\r\n> 1 box of imported chocolates at 11,25\r\n";
+            bool condition = basketContent == richTextBoxContent;
+            Assert.IsTrue(condition);
+        }
+
+        [TestMethod]
+        public void ContentNewBasket()
+        {
+            string basketContent = "";
+            string richTextBoxContent = "";
+            bool condition = basketContent == richTextBoxContent;
+            Assert.IsTrue(condition);
         }
     }
 }
+
