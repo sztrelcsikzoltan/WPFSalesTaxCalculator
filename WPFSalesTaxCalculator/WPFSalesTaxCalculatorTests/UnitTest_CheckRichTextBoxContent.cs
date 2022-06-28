6dfaa3d63275e45e2eaefcb89951e3dd390039bc
sztrelcsikzoltan <sztrelcsikz@kkszki.hu>
2022-06-28 11:56:58 +0200

    Change test methods to use computed shopping basket data - for comparison, we use the data written into the richTextBox (by ShowSampleBasket) and read out from it

--- WPFSalesTaxCalculator/WPFSalesTaxCalculatorTests/UnitTest_CheckRichTextBoxContent.cs
+++ WPFSalesTaxCalculator/WPFSalesTaxCalculatorTests/UnitTest_CheckRichTextBoxContent.cs
@@ -1,17 +1,30 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Collections.Generic;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using WPFSalesTaxCalculator.Classes;
 
 namespace WPFSalesTaxCalculatorTests
 {
     [TestClass]
     public class UnitTest_CheckRichTextBoxContent
     {
+        Methods method = new Methods();
+        RichTextBox richTextBox = new RichTextBox();
+        List<Item> itemsList1 = new List<Item>() { new Item(1, "book", 1, 12.49), new Item(2, "music CD", 1, 14.99), new Item(3, "chocolate bar", 1, 0.85) };
+        List<Item> itemsList2 = new List<Item>() { new Item(1, "imported box of chocolates", 1, 10.00), new Item(2, "imported bottle of perfume", 1, 47.50) };
+        List<Item> itemsList3 = new List<Item>() { new Item(1, "imported bottle of perfume", 1, 27.99), new Item(1, "bottle of perfume", 1, 18.99), new Item(2, "packet of headache pills", 1, 9.75), new Item(3, "box of imported chocolates", 1, 11.25) };
+        List<Item> itemsListNew = new List<Item>();
+
         [TestMethod]
         public void ContentSampleBasket1()
         {
             // in WPF RichTextBox, the lines are separated by the '\r\n>' characters, so it must be considered
-            string basketContent = "> 1 book at 12,49\r\n> 1 music CD at 14,99\r\n> 1 chocolate bar at 0,85\r\n";
-            string richTextBoxContent = "> 1 book at 12,49\r\n> 1 music CD at 14,99\r\n> 1 chocolate bar at 0,85\r\n";
+            // string basketContent = "> 1 book at 12,49\r\n> 1 music CD at 14,99\r\n> 1 chocolate bar at 0,85\r\n";
+            // string richTextBoxContent = "> 1 book at 12,49\r\n> 1 music CD at 14,99\r\n> 1 chocolate bar at 0,85\r\n";
+            string basketContent = method.ShowSampleBasket(richTextBox, itemsList1); // data written into richTextBox
+            string richTextBoxContent = new TextRange(richTextBox.Document.ContentStart, richTextBox.Document.ContentEnd).Text; // data read out from richTextBox
             bool condition = basketContent == richTextBoxContent;
             Assert.IsTrue(condition);
         }
@@ -20,8 +33,10 @@ namespace WPFSalesTaxCalculatorTests
         public void ContentSampleBasket2()
         {
             // in WPF RichTextBox, the lines are separated by the '\r\n>' characters, so it must be considered
-            string basketContent = "> 1 imported box of chocolates at 10,00\r\n> 1 imported bottle of perfume at 47,50\r\n";
-            string richTextBoxContent = "> 1 imported box of chocolates at 10,00\r\n> 1 imported bottle of perfume at 47,50\r\n";
+            // string basketContent = "> 1 imported box of chocolates at 10,00\r\n> 1 imported bottle of perfume at 47,50\r\n";
+            // string richTextBoxContent = "> 1 imported box of chocolates at 10,00\r\n> 1 imported bottle of perfume at 47,50\r\n";
+            string basketContent = method.ShowSampleBasket(richTextBox, itemsList2); // data written into richTextBox
+            string richTextBoxContent = new TextRange(richTextBox.Document.ContentStart, richTextBox.Document.ContentEnd).Text; // data read out from richTextBox
             bool condition = basketContent == richTextBoxContent;
             Assert.IsTrue(condition);
         }
@@ -30,8 +45,10 @@ namespace WPFSalesTaxCalculatorTests
         public void ContentSampleBasket3()
         {
             // in WPF RichTextBox, the lines are separated by the '\r\n>' characters, so it must be considered
-            string basketContent = "> 1 imported bottle of perfume at 27,99\r\n> 1 bottle of perfume at 18,99\r\n> 1 packet of headache pills at 9,75\r\n> 1 box of imported chocolates at 11,25\r\n";
-            string richTextBoxContent = "> 1 imported bottle of perfume at 27,99\r\n> 1 bottle of perfume at 18,99\r\n> 1 packet of headache pills at 9,75\r\n> 1 box of imported chocolates at 11,25\r\n";
+            // string basketContent = "> 1 imported bottle of perfume at 27,99\r\n> 1 bottle of perfume at 18,99\r\n> 1 packet of headache pills at 9,75\r\n> 1 box of imported chocolates at 11,25\r\n";
+            // string richTextBoxContent = "> 1 imported bottle of perfume at 27,99\r\n> 1 bottle of perfume at 18,99\r\n> 1 packet of headache pills at 9,75\r\n> 1 box of imported chocolates at 11,25\r\n";
+            string basketContent = method.ShowSampleBasket(richTextBox, itemsList3); // data written into richTextBox
+            string richTextBoxContent = new TextRange(richTextBox.Document.ContentStart, richTextBox.Document.ContentEnd).Text; // data read out from richTextBox
             bool condition = basketContent == richTextBoxContent;
             Assert.IsTrue(condition);
         }
@@ -39,8 +56,10 @@ namespace WPFSalesTaxCalculatorTests
         [TestMethod]
         public void ContentNewBasket()
         {
-            string basketContent = "";
-            string richTextBoxContent = "";
+            // string basketContent = "";
+            // string richTextBoxContent = "";
+            string basketContent = method.ShowSampleBasket(richTextBox, itemsListNew); // data written into richTextBox
+            string richTextBoxContent = new TextRange(richTextBox.Document.ContentStart, richTextBox.Document.ContentEnd).Text; // data read out from richTextBox
             bool condition = basketContent == richTextBoxContent;
             Assert.IsTrue(condition);
         }
