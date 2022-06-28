740d1322c10905a6d476bf8445e0a0a938b22047
sztrelcsikzoltan <sztrelcsikz@kkszki.hu>
2022-06-28 20:27:46 +0200

    Change test methods to use ShowOutputBasket method - change static actual values to real values returned by the ShowOutputeBasket method - in order to test the method, the ListBox control and the lists used as parameters had to be implemented in the test class - tests were successful - we tested the new basket with empty input data; in this case the method does not even run and the application asks the user to enter valid data

--- WPFSalesTaxCalculator/WPFSalesTaxCalculatorTests/UnitTest_ShowOutputBasket.cs
+++ WPFSalesTaxCalculator/WPFSalesTaxCalculatorTests/UnitTest_ShowOutputBasket.cs
@@ -1,16 +1,27 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Collections.Generic;
+using System.Windows.Controls;
+using WPFSalesTaxCalculator.Classes;
 
 namespace WPFSalesTaxCalculatorTests
 {
     [TestClass]
     public class UnitTest_ShowOutputBasket
     {
-         [TestMethod]
+        Methods method = new Methods();
+        ListBox listBox = new ListBox();
+        List<Item> itemsList1 = new List<Item>() { new Item(1, "book", 1, 12.49), new Item(2, "music CD", 1, 14.99), new Item(3, "chocolate bar", 1, 0.85) };
+        List<Item> itemsList2 = new List<Item>() { new Item(1, "imported box of chocolates", 1, 10.00), new Item(2, "imported bottle of perfume", 1, 47.50) };
+        List<Item> itemsList3 = new List<Item>() { new Item(1, "imported bottle of perfume", 1, 27.99), new Item(1, "bottle of perfume", 1, 18.99), new Item(2, "packet of headache pills", 1, 9.75), new Item(3, "box of imported chocolates", 1, 11.25) };
+        List<Item> itemsListNew = new List<Item>();
+
+        [TestMethod]
         public void OutputSampleBasket1()
         {
             // the output with decimal symbol ',' or '.' can be correct depending on the actual regional settings
-            string actual = "> 1 book: 12,49\n> 1 music CD: 16,49\n> 1 chocolate bar: 0,85\n> Sales taxes: 1,50\n> Total: 29,83\n";
+            // string actual = "> 1 book: 12,49\n> 1 music CD: 16,49\n> 1 chocolate bar: 0,85\n> Sales taxes: 1,50\n> Total: 29,83\n";
+            string actual = method.showOutputBasket(listBox, itemsList1);
             bool condition = actual =="> 1 book: 12,49\n> 1 music CD: 16,49\n> 1 chocolate bar: 0,85\n> Sales taxes: 1,50\n> Total: 29,83\n" || actual == "> 1 book: 12.49\n> 1 music CD: 16.49\n> 1 chocolate bar: 0.85\n> Sales taxes: 1.50\n> Total: 29.83\n";
             Assert.IsTrue(condition);
         }
@@ -19,7 +30,8 @@ namespace WPFSalesTaxCalculatorTests
         public void OutputSampleBasket2()
         {
             // the output with decimal symbol ',' or '.' can be correct depending on the actual regional settings
-            string actual = "> 1 imported box of chocolates: 10,50\n> 1 imported bottle of perfume: 54,65\n> Sales taxes: 7,65\n> Total: 65,15\n";
+            // string actual = "> 1 imported box of chocolates: 10,50\n> 1 imported bottle of perfume: 54,65\n> Sales taxes: 7,65\n> Total: 65,15\n";
+            string actual = method.showOutputBasket(listBox, itemsList2);
             bool condition = actual == "> 1 imported box of chocolates: 10,50\n> 1 imported bottle of perfume: 54,65\n> Sales taxes: 7,65\n> Total: 65,15\n" || actual == "> 1 imported box of chocolates: 10.50\n> 1 imported bottle of perfume: 54.65\n> Sales taxes: 7.65\n> Total: 65.15\n";
             Assert.IsTrue(condition);
         }
@@ -28,7 +40,8 @@ namespace WPFSalesTaxCalculatorTests
         public void OutputSampleBasket3()
         {
             // the output with decimal symbol ',' or '.' can be correct depending on the actual regional settings
-            string actual = "> 1 imported bottle of perfume: 32,19\n> 1 bottle of perfume: 20,89\n> 1 packet of headache pills: 9,75\n> 1 box of imported chocolates: 11,85\n> Sales taxes: 6,70\n> Total: 74,68\n";
+            // string actual = "> 1 imported bottle of perfume: 32,19\n> 1 bottle of perfume: 20,89\n> 1 packet of headache pills: 9,75\n> 1 box of imported chocolates: 11,85\n> Sales taxes: 6,70\n> Total: 74,68\n";
+            string actual = method.showOutputBasket(listBox, itemsList3);
             bool condition = actual == "> 1 imported bottle of perfume: 32,19\n> 1 bottle of perfume: 20,89\n> 1 packet of headache pills: 9,75\n> 1 box of imported chocolates: 11,85\n> Sales taxes: 6,70\n> Total: 74,68\n" || actual == "> 1 imported bottle of perfume: 32.19\n> 1 bottle of perfume: 20.89\n> 1 packet of headache pills: 9.75\n> 1 box of imported chocolates: 11.85\n> Sales taxes: 6.70\n> Total: 74.68\n";
             Assert.IsTrue(condition);
         }
@@ -36,7 +49,9 @@ namespace WPFSalesTaxCalculatorTests
         [TestMethod]
         public void OutputNewBasket()
         {
-            string actual = "";
+            string richTextBoxContent = "";
+            // if there is no content in the richtextBox, the method will not run and the output will remain empty;
+            string actual = richTextBoxContent == "" ? "" : method.showOutputBasket(listBox, itemsListNew);
             bool condition = actual == "";
             Assert.IsTrue(condition);
         }
